d21039794af8858364c30a63dfcabe609f02b87a
Habeeb <habeebabayomi6@gmail.com>
2022-09-08 16:08:29 -0700

    vv

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *  main - Entry point
+ *  Description:'the program's description'
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[100] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+	return (0);
+}
