5e5d2440f52d0c30424ce8c9641516e06250179a
Habeeb <habeebabayomi6@gmail.com>
2022-09-13 16:39:10 -0700

    00

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - check the code 
+ * main - check the code
  *
- * Return : Always 0
+ * Return: Always 0.
  */
-
-int main (void)
+int main(void)
 {
-printf ("_putchar\n");
-
-return (0);
+	printf("_putchar\n");
+	return (0);
 }
